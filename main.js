affcc38c1a6cc74d0ad8cde8735f67dbf2ab4054
Shenhaoyi <shenhaoyi04@163.com>
2024-01-13 20:12:10 +0800

    基于原生api进行实现

---
+++ main.js
@@ -0,0 +1,6 @@
+const app = document.createElement('div');
+app.id = 'app';
+const root = document.getElementById('root');
+root.appendChild(app);
+const text = document.createTextNode('app');
+app.appendChild(text);
