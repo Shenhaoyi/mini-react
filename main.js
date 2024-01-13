7614d68c4e7fff5b00c5627094924122b3ff1be7
Shenhaoyi <shenhaoyi04@163.com>
2024-01-13 21:23:07 +0800

    拆分 App.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import ReactDOM from './core/ReactDOM.js';
-import React from './core/React.js';
+import App from './App.js';
 
-const appVNode = React.createElement('div', { id: 'app' }, 'app');
-
-ReactDOM.createRoot(document.getElementById('root')).render(appVNode);
+ReactDOM.createRoot(document.getElementById('root')).render(App);
