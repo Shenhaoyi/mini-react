8729aff92ff12e2218da7008dc238e1126d0fcc8
Shenhaoyi <shenhaoyi04@163.com>
2024-01-13 20:23:50 +0800

    引入静态虚拟DOM

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-const app = document.createElement('div');
-app.id = 'app';
+const textVNode = {
+  type: 'TEXT_NODE',
+  props: {
+    text: 'app',
+    children: [],
+  },
+};
+
+const appVNode = {
+  type: 'div',
+  props: {
+    id: 'app',
+    children: [textVNode],
+  },
+};
+
+const app = document.createElement(appVNode.type);
 const root = document.getElementById('root');
 root.appendChild(app);
-const text = document.createTextNode('app');
+const text = document.createTextNode(textVNode.props.text);
 app.appendChild(text);
