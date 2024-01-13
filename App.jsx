88b503cdb7a8a92083230df0c1723ed6ec440377
Shenhaoyi <shenhaoyi04@163.com>
2024-01-13 22:07:10 +0800

    安装 vite 来解析 jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from './core/React.js';
+
+// const App = React.createElement('div', { id: 'app' }, 'app');
+const App = <div id="app">app</div>;
+
+// 查看 jsx 被编译结果
+// function test() {
+//   return <div id="app">app</div>;
+// }
+// console.log(test);
+
+export default App;
